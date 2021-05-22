91c90952ec88c1a84013e602273c7dfb6c2b75f3
Andrei Dzebysh <presnia@yahoo.com>
2021-05-22 20:17:55 +0300

    Created the Expenses component and all data of expenses moved there

--- src/App.js
+++ src/App.js
@@ -1,50 +1,28 @@
 import ExpenseItem from "./components/ExpenseItem/ExpenseItem";
+import Expenses from "./components/ExpenseData/ExpenseData";
 
 function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 8, 7) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 27),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
-
   return (
     <div>
-      <h2>Let's get started!</h2>
       <ExpenseItem
-        date={expenses[0].date}
-        title={expenses[0].title}
-        amount={expenses[0].amount}
+        date={Expenses[0].date}
+        title={Expenses[0].title}
+        amount={Expenses[0].amount}
       />
       <ExpenseItem
-        date={expenses[1].date}
-        title={expenses[1].title}
-        amount={expenses[1].amount}
+        date={Expenses[1].date}
+        title={Expenses[1].title}
+        amount={Expenses[1].amount}
       />
       <ExpenseItem
-        date={expenses[2].date}
-        title={expenses[2].title}
-        amount={expenses[2].amount}
+        date={Expenses[2].date}
+        title={Expenses[2].title}
+        amount={Expenses[2].amount}
       />
       <ExpenseItem
-        date={expenses[3].date}
-        title={expenses[3].title}
-        amount={expenses[3].amount}
+        date={Expenses[3].date}
+        title={Expenses[3].title}
+        amount={Expenses[3].amount}
       />
     </div>
   );
--- src/components/ExpenseDate/ExpenseDate.js
+++ src/components/ExpenseDate/ExpenseDate.js
@@ -1,14 +1,16 @@
+import "./ExpenseDate.css";
+
 const ExpenseDate = ({ date }) => {
   const day = date.toLocaleString("us-US", { day: "2-digit" });
   const month = date.toLocaleString("us-US", { month: "long" });
   const year = date.getFullYear();
 
   return (
-    <>
-      <div>{month}</div>
-      <div>{year}</div>
-      <div>{day}</div>
-    </>
+    <div className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{year}</div>
+      <div className="expense-date__day">{day}</div>
+    </div>
   );
 };
 
