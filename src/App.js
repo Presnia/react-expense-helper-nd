51fab15eec7a09b2c65b067d167b702470bd2dc5
Andrei Dzebysh <presnia@yahoo.com>
2021-06-21 18:50:12 +0300

    Created new components NewExpense and ExpenseForm in the new directory

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import Expenses from "./components/Expenses/Expenses";
 import expenses from "./components/Expenses/ExpenseData";
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
